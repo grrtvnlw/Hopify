493f76dba67e2b1d820747bb7e9c87bd6b9c4b8a
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-29 16:02:45 -0400

    map markers are now working

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -11,31 +11,12 @@ class MapContainer extends Component {
   constructor(props) {
     super(props)
   }
-
-  // componentDidMount() {
-  //   let newStores = this.props.map(brewery => {
-  //     brewery.l
-  //   })
-  // }
-
-  // displayMarkers = () => {
-  //   console.log(this.props.breweries)
-  //   this.props.breweries.map((store, index) => {
-  //     console.log(store.latitude)
-  //     console.log(store.longitude)
-  //     return <Marker key={index} id={index} position={{
-  //       lat:  store.latitude ? store.latitude : 33.76333225 ,
-  //       lng:  store.longitude ? store.longitude : -84.3870607355802 
-  //     }}
-  //     onClick={() => console.log("You clicked me!")} />
-  //   })
-  // }
   
   render() {
     return (
         <Map
           google={this.props.google}
-          zoom={8}
+          zoom={10}
           style={mapStyles}
           initialCenter={{ lat: 33.76333225, lng: -84.3870607355802 }}
         >
