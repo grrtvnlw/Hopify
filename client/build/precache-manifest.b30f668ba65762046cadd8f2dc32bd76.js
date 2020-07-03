075c0dc50af2b7d9de2b5382cc6494416c04d946
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-07-03 14:16:41 -0400

    Adding build folder

---
+++ client/build/precache-manifest.b30f668ba65762046cadd8f2dc32bd76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d58a72346f508d69527da8a6689210",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa00a4e1cec8d1c35dea",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "2ba124227f7fd5c69d35",
+    "url": "/static/css/main.e8b023ad.chunk.css"
+  },
+  {
+    "revision": "fa00a4e1cec8d1c35dea",
+    "url": "/static/js/2.0ae7fa01.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/static/js/2.0ae7fa01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba124227f7fd5c69d35",
+    "url": "/static/js/main.741e3264.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2f60bb56cf3f28c4f6f8b4a81ee9dcc7",
+    "url": "/static/media/Hops_IPA_Pour-BA-1200.2f60bb56.jpg"
+  },
+  {
+    "revision": "26054c015b7faa792f60cceb546f9b33",
+    "url": "/static/media/beer.26054c01.png"
+  }
+]);
