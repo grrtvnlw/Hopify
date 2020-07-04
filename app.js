537ccbbd4cf0b591b59fdf343dca3f8961391f41
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-07-03 22:56:55 -0400

    Testing

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ app.get('/random', (req, res) => {
     })
 })
 
+// Testing code for Heroku deploy
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 });
