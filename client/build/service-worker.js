038f7da157ee1abb014afef7e8ff372a59d00755
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2021-03-26 09:09:52 -0400

    new build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.962db6b76933448568df4d6b84206d1c.js"
+  "/precache-manifest.2332322d351b1d1e1c6f45fed12d0d41.js"
 );
 
 self.addEventListener('message', (event) => {
